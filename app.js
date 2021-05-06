231637e644773fa788ee7507770ed201f6c8b4ec
samuel Odubamowo <wemsamuel@gmail.com>
2021-05-06 02:29:53 -0700

    completed user prompt

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const Choices = require("inquirer/lib/objects/choices");
 
 // const fs = require('fs');
 // const { generate } = require('rxjs');
@@ -12,12 +13,140 @@ const inquirer = require("inquirer");
 
 //     console.log("Portfolio complete! check out index.html to see the output");
 // });
-inquirer
-.prompt([
-    {
-        type: "input",
-        name: "name",
-        message: "Whats your name?"
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Whats your name? (Required)",
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                }else{
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your github Username (Required)",
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                }else{
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "confirm",
+            name: "ConfirmAbout",
+            message: "Would you like to add some Information about yourself?",
+            default: true
+        },
+ 
+        {
+            type: "input",
+            name: "about",
+            message: "provide some information about yourself:",
+            when: ({ confirmAbout }) => {
+                if (confirmAbout){
+                    return true;                
+                } else{
+                    return false;
+                }
+            }
+        }
+
+    ]);
+    
+};
+
+const promptProject = portfolioData => {
+    console.log(`
+    ========================
+    Add a New Project
+    ========================
+    `);
+
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
     }
-])
-.then(answer => (console.log(answer)));
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Whats is the name of your project? (Required)",
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                }else{
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Provide a description of the project (Required)",
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                }else{
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "checkbox",
+            name: "languages",
+            message: "What did you build this project with? (check all that applies)",
+            choices: ["JavaScript", "HTML", "CSS", "ES6", "jQuery", "Bootstrap", "Node"]
+        },
+        {
+            type: "input",
+            name: "link",
+            message: "Enter the Github link to your project. (Required)",
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                }else{
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "confirm",
+            name: "feature",
+            message: "Would you like to feature this project?",
+            default: false
+        },
+        {
+            type: "confirm",
+            name: "confirmAddProject",
+            message: "Would you like to enter another project?",
+            default: false
+        }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
+
+};
+
+promptUser()
+    .then(promptProject)    
+    .then(portfolioData => {
+        console.log(portfolioData);
+    })
