c94db96f72c4777dd0b700ac583e7b504b607f3d
samuel Odubamowo <wemsamuel@gmail.com>
2021-05-08 04:30:06 -0700

    completed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const generatePage = require("./src/page-template.js");
 
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 // const { generate } = require('rxjs');
 
 
@@ -188,15 +188,20 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-    .then(promptProject)
-    .then(mockData => {
-        const pageHTML = generatePage(mockData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw new Error(err);
-
-            console.log("Portfolio complete! check out index.html to see the output");
-        });
-
-    })
-
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
